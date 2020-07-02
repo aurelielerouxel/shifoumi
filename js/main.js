253ff7538d704d22ea7af04951c1bd612283b67e
Aurelie Lerouxel <malkiniou@gmail.com>
2020-07-02 15:44:27 +0200

    add welcome message in shifoumi

---
+++ js/main.js
@@ -0,0 +1,28 @@
+//Accueil de l'utilisateur
+alert("Bonjour et bienvenue");
+ 
+//Permettre à l'utilisateur de rentrer et conserver son nom
+//Empêcher une entrée de nom à plus de 20 caractères, condition
+var userName = prompt("Entrez votre nom");
+console.log(userName);
+
+
+    
+//Permettre à l'utilisateur de rentrer une valeur choisie
+//Conditions : if, else,
+
+
+
+//Générer un choix au hasard pour l'ordinateur
+//Math.random
+
+
+//Afficher le choix du joueur et de l'ordinateur
+//Récupérer choix joueur et ordinateur
+
+
+//Déterminer le vainqueur 
+//Comparer choix joueur et ordinateur
+
+
+//Afficher le vainqueur
