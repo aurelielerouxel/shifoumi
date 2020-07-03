00d123edd1f3957d32bde0a6d78f632f5a093e38
Aurelie Lerouxel <malkiniou@gmail.com>
2020-07-03 08:54:26 +0200

    add computer choice in shifoumi

--- js/main.js
+++ js/main.js
@@ -1,26 +1,27 @@
 //Accueil de l'utilisateur
 alert("Bonjour et bienvenue");
-
  
 //Permettre à l'utilisateur de rentrer et conserver son nom
 var userName = prompt("Entrez votre nom");
-console.log(userName);
 
- 
 //Permettre à l'utilisateur de rentrer une valeur choisie
 var userChoice = prompt("Faites votre choix : pierre, feuille, ciseau");
-console.log(userChoice);
-
 
 //Générer un choix au hasard pour l'ordinateur
-//(Math.random)
-
+var choices = ["pierre", "feuille", "ciseaux"];
+ 
+function choiceAleatoire() {
+    var randomValue = choices[Math.floor(Math.random() * choices.length)];
+    return randomValue;
+}
+var computerChoice = choiceAleatoire();
 
 //Afficher le choix du joueur et de l'ordinateur
+
 //Récupérer choix joueur et ordinateur
 
-
 //Déterminer le vainqueur 
+
 //Comparer choix joueur et ordinateur
 
 
